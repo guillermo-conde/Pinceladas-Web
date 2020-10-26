35b9c26647d91f3683bca74d10d26321a5135b9e
Universidad Politécnica de Tlaxcala <condeguillermo8@gmail.com>
2020-10-25 21:08:19 -0600

    Desintegracion de Codeigniter

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="assets/recursos/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<link rel="stylesheet" href="assets/css/estilos.css">
+	<title>Pinceladas Ceramica</title>
+</head>
+<body>
+	<header>
+		<section class="inicio">
+			<div class="tituloPagina">
+				<h1>
+					Pinceladas Cerámica
+				</h1>
+			</div>
+			<div class="menuPrincipal">
+				<nav>
+					<ul>
+						<li class="itemPrinc"><a href="" >Sobre nosotros</a></li>
+						<li class="itemPrinc"><a href="" >Nuestros Productos</a>
+							<ul class="categorias">
+								<li class="itemSecun"><a href="">Alcancías</a></li>
+								<li class="itemSecun"><a href="">Alhajeros</a></li>
+								<li class="itemSecun"><a href="">Cocina</a></li>
+								<li class="itemSecun"><a href="">Hogar</a></li>
+								<li class="itemSecun"><a href="">Floreros</a></li>
+								<li class="itemSecun"><a href="">Maceteros</a></li>
+								<li class="itemSecun"><a href="">Recuerdos</a></li>
+								<li class="itemSecun"><a href="">Tlaxcala</a></li>
+								<li class="itemSecun"><a href="">Fechas especiales</a>
+									<ul class="fEspeciales">
+										<li class="itemTerc"><a href="">Día de muertos</a></li>
+										<li class="itemTerc"><a href="">Fiestas patrias</a></li>
+										<li class="itemTerc"><a href="">Navidad</a></li>
+										<li class="itemTerc"><a href="">San Valentin</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li class="itemPrinc"><a href="">Contacto</a></li>
+					</ul>
+				</nav>
+			</div>
+		</section>
+		<img src="assets/recursos/logo2.jpg"  class="portada" alt="">
+	</header>
+
+	<section>
+		<div class="bienvenida">
+			<h2>Bienvenido a Pinceladas</h2>
+			<p>(Colocar eslogan)</p>
+		</div>
+		<div class="masVendido">
+			<h2>Lo mas vendido</h2>
+			<img src="assets/img/1.jpg" alt="" class="img">
+			<img src="assets/img/2.jpg" alt="" class="img">
+			<img src="assets/img/3.jpg" alt="" class="img">
+		</div>
+		<div class="conocenos">
+			<h2 class="subtitulo">Conócenos</h2>
+			<div class="mision">
+				<h3 class="subtitulo">Mision</h3>
+				<p>(Mision)</p>
+			</div>
+			<div class="vision">
+				<h3 class="subtitulo">Vision</h3>
+				<p>(Vision)</p>
+			</div>
+			<div>
+				<div class="contacto">
+					<h3 class="subtitulo">Contacto</h3>
+					<p><b>Teléfono: </b>(Colocar telefono)</p>
+					<p><b>Mail: </b>(Colocar mail)</p>
+					<p><b>Horario de atencion: </b>(Colocar horario)</p>
+				</div>
+				<div class="ubicacion">
+					<p>(Espacio para la ubicacion de google maps)</p>
+				</div>
+			</div>
+			
+		</div>
+		<div>
+			<div class="puntosEntrega">
+				<h2 class="subtitulo">Entregamos en:</h2>
+				<p><b>Direccion</b></p>
+				<p><b>Horario</b></p>
+			</div>
+			<div class="ubicacion">
+				(Espacio para la ubicacion de google maps)
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div><p><b>Pinceladas Cerámica.</b> Todos los derechos reservados.</p></div>
+		<div><p>Consulta <a href="">aquí</a> nuestro aviso de privacidad.</p></div>
+	</footer>
+</body>
+  
+</html>
